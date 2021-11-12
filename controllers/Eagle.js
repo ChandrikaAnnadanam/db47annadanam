617b3a9fb4f723df5279ac12c19568d20fa54121
ChandrikaAnnadanam <chandrikaannadanam@gmail.com>
2021-11-12 10:08:57 -0600

    Update Eagle.js

--- controllers/Eagle.js
+++ controllers/Eagle.js
@@ -4,9 +4,16 @@ exports.Eagle_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Eagle list');
 };
 // for a specific Eagle.
-exports.Eagle_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Eagle detail: ' + req.params.id);
-};
+exports.Eagle_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Eagle.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle Eagle create on POST.
 exports.Eagle_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: Eagle create POST');
@@ -62,3 +69,22 @@ exports.Eagle_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   exports.Eagle_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Eagle.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Age)  
+               toUpdate.Age = req.body.Age; 
+        if(req.body.colour) toUpdate.colour = req.body.colour; 
+        if(req.body.weight) toUpdate.weight = req.body.weight; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
